7e04c401d2622dfe4a6b3aad01398874aa43e71a
Chriiiiiss <chris.fraanc@gmail.com>
2025-10-16 18:11:19 +0200

    chore(player): add player cooldown on shoot because it's too much op for now

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.math import Vector2
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_SHOOT_COOLDOWN, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED
 from shot import Shot
 
 class Player(CircleShape):
@@ -28,6 +28,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.cooldown -= dt
 
         if keys[pygame.K_q]:
             self.rotate(-dt)
@@ -38,7 +39,10 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
+            if self.cooldown > 0:
+                return
             self.shoot()
+            self.cooldown = PLAYER_SHOOT_COOLDOWN
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
