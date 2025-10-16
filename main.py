6d56dbe6eb9a347393c21d9b2e2cac49daa040cf
Chriiiiiss <chris.fraanc@gmail.com>
2025-10-16 15:09:01 +0200

    feat(player): add player movement !

--- main.py
+++ main.py
@@ -16,10 +16,12 @@ def main():
                 return
 
         screen.fill("black")
+        player.update(dt)
+
         player.draw(screen)
 
         pygame.display.flip()
-        dt = clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
