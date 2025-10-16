547229d5b2ead5e5f48d3f7475231fd6f56e5d5f
Chriiiiiss <chris.fraanc@gmail.com>
2025-10-16 15:18:59 +0200

    chore(main): add groups to have a better sprite management

--- main.py
+++ main.py
@@ -8,17 +8,23 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable_group) #type: ignore
+
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
-        player.update(dt)
+        updatable.update(dt)
 
-        player.draw(screen)
+        screen.fill("black")
+        for drawable_item in drawable_group:
+            drawable_item.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
